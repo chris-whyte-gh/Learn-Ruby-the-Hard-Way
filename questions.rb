e0992b0c3f31ad25c596135d29ded588ed6a2ec8
Christopher Whyte <cwhyte@rsglab.com>
2018-08-18 15:35:45 -0400

    Added questions.rb

---
+++ questions.rb
@@ -0,0 +1,15 @@
+print "How old are you? "
+age = gets.chomp
+
+print "How tall are you? "
+height = gets.chomp
+
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall, and #{weight} heavy."
+
+print "Are you learning ruby? "
+ruby = gets.chomp.capitalize
+
+puts "And are you learning code? #{ruby}"
