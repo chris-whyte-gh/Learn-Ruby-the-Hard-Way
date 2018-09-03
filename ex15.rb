7d111a5b62716cd37fc2e5357c2e0ec4ff8b2246
Christopher Whyte <cwhyte@rsglab.com>
2018-09-03 16:21:48 -0400

    added comments

--- ex15.rb
+++ ex15.rb
@@ -2,12 +2,12 @@
 
 filename = ARGV.first #Uses argv to get a file name, just the first thing submitted
 
-txt = open(filename)
+txt = open(filename) #open is a command/function/method
 
-puts "Here's your file #{filename}:"
-print txt.read
+puts "Here's your file #{filename}:" #puts filename name
+print txt.read #runs function read on txt variable
 
-print "Type the filename again: "
+print "Type the filename again: " #does the same thing as above with a different variable name
 file_again = $stdin.gets.chomp
 
 txt_again = open(file_again)
