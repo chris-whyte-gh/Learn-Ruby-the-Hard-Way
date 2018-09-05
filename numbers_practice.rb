e3bb083aa5d0055f6c859ae6ced4be5d8e2ebae8
Christopher Whyte <cwhyte@rsglab.com>
2018-09-05 06:57:11 -0400

    adding all files

--- numbers_practice.rb
+++
@@ -1,7 +0,0 @@
-print "Give me a number: "
-number = gets.chomp.to_f
-
-discount = number * 0.1
-#uses round(2) method to round the float to two decimal places
-puts "Your discount is #{discount.round(2)}"
-
