1ef8f093c480a0115b38f52b11bcabd5ed6b1928
Christopher Whyte <cwhyte@rsglab.com>
2018-09-11 09:28:46 -0400

    exercise 17

--- ex17/ex17.rb
+++ ex17/ex17.rb
@@ -2,21 +2,24 @@ from_file, to_file = ARGV
 
 puts "Copying from #{from_file} to #{to_file}"
 
+#open first file, from_file, save it to variable in_file, read that and save it to indata
 in_file = open(from_file)
 indata = in_file.read
-#to do this in one line
-#indata = open(from_file, 'r')
 
 puts "The input file is #{indata.length} bytes long"
 
+#call exist? method of File object with variable to_file
 puts "Does the output file exist? #{File.exist?(to_file)}"
 puts "Ready, hit RETURN to continue, CTRL-C to abort."
+#waiting to get user input
 $stdin.gets
 
+#open to_file, make it writable, save to variable out_file
 out_file = open(to_file, 'w')
+#write contents of indata to out_file
 out_file.write(indata)
 
-puts "Alright, all done"
+puts "Alright, all done."
 
 out_file.close
 in_file.close
