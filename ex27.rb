580bf6526ba1f91b6e36d5b98d16204c36723945
Christopher Whyte <cwhyte@rsglab.com>
2018-10-24 07:30:03 -0400

    Progress so far

---
+++ ex27.rb
@@ -0,0 +1,21 @@
+&& = and
+|| = or
+! = not
+!= = not equal
+== = equal
+>= = greater than equal
+<= = less than equal
+true
+false
+
+----
+!false = true
+!true = false
+
+-------
+true || false = true
+true || true = true
+false || true = true
+false || false = false
+
+-------
