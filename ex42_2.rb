0b6f1c01c02ff688ad1d32349c4e12e63c557b85
Christopher Whyte <cwhyte@rsglab.com>
2019-02-21 06:59:23 -0500

    Finished up practice file ex42_2

--- ex42_2.rb
+++ ex42_2.rb
@@ -21,21 +21,58 @@ zoe = Dog.new("Zoe")
 zoe.speak()
 
 class Person < Animal
-    attr_accessor :name, :pet, :salary
+    
+    def initialize(name) #Person has-a name
+        @name = name
+
+        @pet = nil #person has-a pet
+
+        @hobbies = [] #person has-a list of hobbies [array]
+
+        @preferences = {} #Dietary preferences, key>value hash
+    end
+    
+    attr_accessor :pet, :hobbies, :preferences, :salary
+    
+    def describe()
+        puts "This person's name is #{@name}."
+        puts "#{@name}'s hobbies include: "
+
+        @hobbies.each {|hobby| puts hobby}
+
+        puts "#{@name}'s food preferences are: "
+
+        @preferences.each { |key, value| puts "#{key}: #{value}"}
+
+        puts "#{@name} has a pet named #{@pet.name}." unless @pet.nil?
+    end 
     
     def education 
         puts "I am so smart"
     end
+
 end
 
-chris = Person.new
+chris = Person.new("Chris")
 chris.pet = "Zoe"
 puts chris.pet
 
 class Student < Person
 end 
 
-donny = Student.new
+donny = Student.new("Donny")
 donny.salary = 140000
 puts donny.salary
 puts donny.education
+
+richard = Person.new("Richard")
+dusty = Dog.new("Dusty")
+richard.hobbies = ["dj","swimming","running"]
+richard.preferences = {
+    "diet" => "vegan",
+    1 => "many",
+    "pizza" => "always" 
+}
+richard.pet = dusty #How do I make richard have a pet named "mittens"
+
+richard.describe()
